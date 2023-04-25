99d9a093d74065ee4ea5b812317ad078cd99a606
user67cl3 <user67cl3@ldap.com>
2023-04-25 19:46:27 +0530

    added

---
+++ AzureCloudProviders.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.74.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  /*
+    Multi line comment
+  */
+  features {}
+
+
+}
