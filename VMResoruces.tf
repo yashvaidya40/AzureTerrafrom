71d1ce31427d194cb8f2538c9969e8dddbba374c
Shubham-Bawa <51166582+Shubham-Bawa@users.noreply.github.com>
2024-03-07 15:34:29 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -37,7 +37,7 @@ resource "azurerm_virtual_machine" "vm1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "shubham2361"
+  name                = "hemant"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "TerraformRG"
@@ -45,7 +45,7 @@ resource "azurerm_virtual_network" "vnet1" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "vishnusubnet11"
+  name                 = "hrmante"
   resource_group_name  = "TerraformRG"
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
