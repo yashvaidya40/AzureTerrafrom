7c750bbf62c8438e741a4bc859ddafb9b47fe719
yashvaidya40 <95998278+yashvaidya40@users.noreply.github.com>
2024-05-28 10:37:27 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name                  = "testetstetestvm"
+  name                  = "testetstetestvm2"
   resource_group_name   = "TerraformRG"
   location              = "East US"
   network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
   storage_os_disk {
-    name = "vishnudisk1"
+    name = "vishnudisk21"
     caching = "ReadWrite"
     managed_disk_type = "Standard_LRS"
     create_option = "FromImage"
@@ -37,7 +37,7 @@ resource "azurerm_virtual_machine" "vm1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "testetstetest"
+  name                = "testetstetest2"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "TerraformRG"
@@ -45,19 +45,19 @@ resource "azurerm_virtual_network" "vnet1" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "hrmante"
+  name                 = "hrmante2"
   resource_group_name  = "TerraformRG"
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "vishnunic111"
+  name                = "vishnunic1112"
   location            = "East US"
   resource_group_name = "TerraformRG"
 
   ip_configuration {
-    name                          = "config111"
+    name                          = "config1121"
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id =azurerm_public_ip.public1.id
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "nic1" {
 }
 
 resource "azurerm_public_ip" "public1" {
-  name = "testetstetest-tywteywgjh"
+  name = "testetstetest-tywteywgjh2"
   resource_group_name = "TerraformRG"
   location = "East US"
   allocation_method = "Static"
